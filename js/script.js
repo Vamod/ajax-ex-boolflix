6178ee44efd20878abb2abfe1873b7caee15df7c
Valerio <valerio.modesti@gmail.com>
2020-09-02 15:09:14 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -1,24 +1,37 @@
 $(document).ready(function(){
+
     $('#myButton').click(function(){
         var movie = $('#myInput').val();
-
+        $('.movie-list').empty();
 
 
         $.ajax(
           {
-            url: 'https://api.themoviedb.org/3/search/movie?api_key=bb992b815f7bd546c69001e27b586501&query=' + movie,
+            url: 'https://api.themoviedb.org/3/search/movie',
             method: 'GET',
+            data:{
+                api_key: 'bb992b815f7bd546c69001e27b586501',
+                language: 'it-IT',
+                query: movie
+            },
             success: function(risposta){
                 console.log(risposta.results);
+
                 var source = $("#entry-template").html();
                 var template = Handlebars.compile(source);
 
                 for(var i = 0; i < risposta.results.length; i++){
-                    var html = template(risposta.results[i]);
-                    $('.container').append(html);;
-
+                    var context = {
+                        title: risposta.results[i].title,
+                        original_title: risposta.results[i].original_language,
+                        original_language: risposta.results[i].vote_average
+                    }
+                    var html = template(context);
+                    $('.movie-list').append(html);;
                 }
 
+                //per pulire campo di input
+                $('#myInput').val(' ');
 
             },
             error: function(){
