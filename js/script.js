85b3868121b7274c92fb736765e6c04a72775ef9
Valerio <valerio.modesti@gmail.com>
2020-09-02 12:02:38 +0200

    dati non filtrati

--- js/script.js
+++ js/script.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
     $('#myButton').click(function(){
         var movie = $('#myInput').val();
 
+
+
         $.ajax(
           {
             url: 'https://api.themoviedb.org/3/search/movie?api_key=bb992b815f7bd546c69001e27b586501&query=' + movie,
             method: 'GET',
             success: function(risposta){
+                console.log(risposta.results);
+                var source = $("#entry-template").html();
+                var template = Handlebars.compile(source);
 
-                console.log(risposta);
+                for(var i = 0; i < risposta.results.length; i++){
+                    var html = template(risposta.results[i]);
+                    $('.container').append(html);;
+
+                }
+
+
             },
             error: function(){
               alert('errore!');
